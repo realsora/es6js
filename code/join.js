b3ddb5ad64b8ad3bb52e22983570d6838883876a
realsora <realsora@gmail.com>
2020-01-17 16:21:55 +0000

    lesson 7

---
+++ code/join.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _ = {
+  // Implements:
+  // https://lodash.com/docs#join
+  join: (array, separator = ',') => {
+    // write code to implement method here
+  }
+}
+
+
+const value = _.join(["hello", "goodbye"], ", ")
+
+console.log(value);
--- code/runner.js
+++ code/runner.js
@@ -1,4 +1,4 @@
 import adder from './adder';
 import {DIGITS} from './adder';
 
-console.log(adder(2));
+console.log("12 is " + DIGITS + " away from " + adder(12));
